31b066160d44761cc41e79e194bd99f2050b93c2
Jason Michelson <jasontmichelson@gmail.com>
2021-12-07 21:08:04 -0700

    Add README.md, remove unused comments, remove unused files.

---
+++ README.md
@@ -0,0 +1,9 @@
+# C++ Workspace
+## Summary
+* A bunch of random examples of C++ code.
+## Functional C++
+* Functional C++
+* Template Metaprogramming (TMP)
+* Reactive C++ (RxCpp)
+* Lambdas / Anonymous functions
+* Boost.Hana C++ Metaprogramming
--- transform_copy_acculumlate/main.cpp
+++ transform_copy_acculumlate/main.cpp
@@ -1,5 +1,3 @@
-//#define CATCH_CONFIG_MAIN
-//#include "catch.hpp"
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -37,22 +35,3 @@ int main() {
 
     return 0;
 }
-
-/*
-int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-TEST_CASE("Fibonacci series", "[fibonacci]") {
-    REQUIRE(fibonacci(0) == 0);
-    REQUIRE(fibonacci(1) == 1);
-    REQUIRE(fibonacci(2) == 1);
-    REQUIRE(fibonacci(3) == 2);
-    REQUIRE(fibonacci(4) == 3);
-    REQUIRE(fibonacci(5) == 5);
-    REQUIRE(fibonacci(12) == 144);
-}
-*/
